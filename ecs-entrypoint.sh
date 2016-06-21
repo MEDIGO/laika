3f8a0ef7aa66cc5fd7e739bc46c33ac218b4cbb6
Miguel Bernardes <mrsbernardes@gmail.com>
2016-06-21 18:32:56 +0200

    *tp2067 - Add Circle CI config

--- ecs-entrypoint.sh
+++ ecs-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-# workarround for: https://forums.aws.amazon.com/message.jspa?messageID=671321
+# workaround for: https://forums.aws.amazon.com/message.jspa?messageID=671321
 export LAIKA_STATSD_HOST=$(curl -s 169.254.169.254/latest/meta-data/local-ipv4)
 
 exec "$@"
