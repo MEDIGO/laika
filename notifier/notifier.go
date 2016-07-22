f9c2c71a114a96f66b1085b4a815a8a0e6048620
Diego Barberá <diego.barbera@medigo.com>
2016-07-22 13:02:25 +0200

    Improve Slack notifications

--- notifier/notifier.go
+++ notifier/notifier.go
@@ -1,5 +1,7 @@
 package notifier
 
+// Notifier describes a notification sender.
 type Notifier interface {
-	NotifyStatusChange(featureName string, status bool, environmentName string) error
+	// NotifyStatusChange notifies a change in the status of a flag.
+	NotifyStatusChange(feature string, status bool, environment string) error
 }
