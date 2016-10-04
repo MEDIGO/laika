1630c647f9be1e0a45b98d9c9a2af6a4d82382de
Diego Barberá <diego.barbera@medigo.com>
2016-10-04 18:38:28 +0200

    Use go-healthz library

--- api/server.go
+++ api/server.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	"github.com/DataDog/datadog-go/statsd"
+	"github.com/MEDIGO/go-healthz"
 	"github.com/MEDIGO/laika/notifier"
 	"github.com/MEDIGO/laika/store"
 	"github.com/labstack/echo"
@@ -48,25 +49,24 @@ func NewServer(conf ServerConfig) (*standard.Server, error) {
 	e.Use(InstrumentMiddleware(conf.Stats))
 	e.Use(middleware.Recover())
 
-	health := NewHealthResource(conf.Store, conf.Stats)
 	features := NewFeatureResource(conf.Store, conf.Stats, conf.Notifier)
 	environments := NewEnvironmentResource(conf.Store, conf.Stats)
 	users := NewUserResource(conf.Store, conf.Stats)
 
-	e.Get("/api/health", echo.HandlerFunc(health.Get))
+	e.Get("/api/health", standard.WrapHandler(healthz.Handler()))
 
-	e.Get("/api/features/:name", echo.HandlerFunc(features.Get), basicAuthMiddleware)
-	e.Get("/api/features", echo.HandlerFunc(features.List), basicAuthMiddleware)
-	e.Post("/api/features", echo.HandlerFunc(features.Create), basicAuthMiddleware)
-	e.Patch("/api/features/:name", echo.HandlerFunc(features.Update), basicAuthMiddleware)
+	e.Get("/api/features/:name", features.Get, basicAuthMiddleware)
+	e.Get("/api/features", features.List, basicAuthMiddleware)
+	e.Post("/api/features", features.Create, basicAuthMiddleware)
+	e.Patch("/api/features/:name", features.Update, basicAuthMiddleware)
 
-	e.Get("/api/environments/:name", echo.HandlerFunc(environments.Get), basicAuthMiddleware)
-	e.Get("/api/environments", echo.HandlerFunc(environments.List), basicAuthMiddleware)
-	e.Post("/api/environments", echo.HandlerFunc(environments.Create), basicAuthMiddleware)
-	e.Patch("/api/environments/:name", echo.HandlerFunc(environments.Update), basicAuthMiddleware)
+	e.Get("/api/environments/:name", environments.Get, basicAuthMiddleware)
+	e.Get("/api/environments", environments.List, basicAuthMiddleware)
+	e.Post("/api/environments", environments.Create, basicAuthMiddleware)
+	e.Patch("/api/environments/:name", environments.Update, basicAuthMiddleware)
 
-	e.Get("/api/users/:username", echo.HandlerFunc(users.Get), basicAuthMiddleware)
-	e.Post("/api/users", echo.HandlerFunc(users.Create), basicAuthMiddleware)
+	e.Get("/api/users/:username", users.Get, basicAuthMiddleware)
+	e.Post("/api/users", users.Create, basicAuthMiddleware)
 
 	e.Static("/", "public")
 
