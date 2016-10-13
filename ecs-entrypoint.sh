46f34cd8d4e4123f38e0543bc5adec43d96b3039
Diego Barberá <diego.barbera@medigo.com>
2016-10-13 23:17:41 +0200

    Remove deployment scripts

--- ecs-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-
-# workaround for: https://forums.aws.amazon.com/message.jspa?messageID=671321
-export LAIKA_STATSD_HOST=$(curl -s 169.254.169.254/latest/meta-data/local-ipv4)
-
-exec "$@"
