5b1d9e452976ceb6dedd5dfc81a5d2981b4d2044
Diego Barberá <diego.barbera@medigo.com>
2016-11-18 15:41:20 +0100

    Fix CircleCI deployment step

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,7 +4,11 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-COMMIT=$(git rev-parse HEAD)
+if [[ -z ${CIRCLE_SHA1:-} ]]; then
+  COMMIT=$(git rev-parse HEAD)
+else
+  COMMIT=${CIRCLE_SHA1}
+fi
 
 docker tag medigo/laika:latest medigo/laika:${COMMIT}
 docker login -e ${DOCKER_EMAIL} -u ${DOCKER_USER} -p ${DOCKER_PASS}
