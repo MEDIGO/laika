f8843a292e9c8a5aacdd1e3f30462be72326b7cf
Paul Baecher <paul.baecher@medigo.com>
2018-02-23 16:02:39 +0100

    Make a proper production build

---
+++ webpack.prod.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MinifyPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './dashboard/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'assets/bundle.js',
+    publicPath: '/',
+  },
+  plugins: [
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new HtmlWebpackPlugin({
+      template: './dashboard/index.html',
+      inject: 'body'
+    }),
+    new MinifyPlugin()
+  ],
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
+  },
+};
