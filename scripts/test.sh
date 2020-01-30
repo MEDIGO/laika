a24fb1451b67d3bfb6d32de78484f7f1863a454b
peterzernia <peter.zernia@medigo.com>
2020-01-30 10:17:17 +0100

    update running tests to new go syntax

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,12 +4,4 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-echo "mode: set" > combined_coverage.out
-
-for d in $(go list ./...); do
-    docker-compose run laika go test -v ./... -cover -coverprofile=coverage.out $d
-    if [ -f coverage.out ]; then
-        cat coverage.out | grep -h -v "^mode:" >> combined_coverage.out
-        rm coverage.out
-    fi
-done
+docker-compose run laika go test -v ./... -cover -coverprofile=combined_coverage.out github.com/MEDIGO/laika
